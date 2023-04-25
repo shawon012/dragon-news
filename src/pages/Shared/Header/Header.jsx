e10c81e796aca88b6e501166fcf72f55be43389a
shawon012 <shawon.shakib01@gmail.com>
2023-04-26 04:02:08 +0600

    right side nav bar done

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -19,28 +19,18 @@ const Header = () => {
             </div>
             <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
                 <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="#features">Features</Nav.Link>
-                            <Nav.Link href="#pricing">Pricing</Nav.Link>
-                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">
-                                    Another action
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
-                            </NavDropdown>
+                        <Nav className="mx-auto">
+                            <Nav.Link href="#features">Home</Nav.Link>
+                            <Nav.Link href="#pricing">About</Nav.Link>
+                            <Nav.Link href="#pricing">Career</Nav.Link>
+                            
                         </Nav>
                         <Nav>
-                            <Nav.Link href="#deets">More deets</Nav.Link>
+                            <Nav.Link href="#deets">Profile</Nav.Link>
                             <Nav.Link eventKey={2} href="#memes">
-                                Dank memes
+                                <Button variant="secondary">Login</Button>
                             </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Button, ListGroup } from 'react-bootstrap';
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import Qzone from '../Qzone/Qzone';
+import bg from '../../../assets/bg.png'
 
 const RightNav = () => {
     return (
         <div>
-            <h2>Right Nav</h2>
+            <h4>Login With</h4>
+            <Button className='mb-2' variant="outline-primary"><FaGoogle />Login With Google</Button>
+            <Button variant="outline-secondary"><FaGithub />  Login With Github</Button>
+            <div>
+                <h4>find Us On</h4>
+                <ListGroup>
+                    <ListGroup.Item><FaFacebook/>Facebook</ListGroup.Item>
+                    <ListGroup.Item><FaTwitter/>Twitter</ListGroup.Item>
+                    <ListGroup.Item><FaInstagram/>Instagram</ListGroup.Item>
+                </ListGroup>
+            </div>
+            <Qzone></Qzone>
+            <div>
+                <img src={bg} alt="" />
+            </div>
         </div>
     );
 };
